0fb1f0e8f1de0a033c32a00b815e9284793aafc4
mustemar <Marcel.Musteata@externals.adidas.com>
2022-07-19 10:11:44 +0300

    refactor services to a common base interface

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic.services;
+
+
+import java.util.Set;
+
+
+public interface CrudService<T, ID> {
+
+  Set<T> findAll ();
+
+
+  T findById (ID id);
+
+
+  T save (T type);
+
+
+  void delete (T object);
+
+
+  void deleteById (ID id);
+
+}
